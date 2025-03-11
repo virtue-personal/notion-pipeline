3ab0c744535cfd6ad853ac85f121848c631499d1
악덕 <rudejrqkr97@gmail.com>
2025-03-12 01:11:57 +0900

    init

---
+++ src/main/java/com/virtue/notionpipeline/NotionPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.virtue.notionpipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotionPipelineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotionPipelineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/virtue/notionpipeline/NotionPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virtue.notionpipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotionPipelineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
